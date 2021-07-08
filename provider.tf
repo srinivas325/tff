1c8603a20ecdf9ea05fdc0ac737b9d5ad5fd581c
srinivas325 <59291856+srinivas325@users.noreply.github.com>
2021-07-08 16:53:17 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   }
